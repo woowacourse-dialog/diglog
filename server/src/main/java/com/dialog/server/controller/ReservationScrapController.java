a7ae7908022abe0085ac248722749bef1e5ac180
LeeJaeHoon <dlwogns3413@naver.com>
2025-05-16 22:01:58 +0900

    refactor: scrap api base url 변경

--- server/src/main/java/com/dialog/server/controller/ReservationScrapController.java
+++ server/src/main/java/com/dialog/server/controller/ReservationScrapController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/reservations/{reservationId}")
+@RequestMapping("/reservations/{reservationId}/scraps")
 @RequiredArgsConstructor
 public class ReservationScrapController {
 
