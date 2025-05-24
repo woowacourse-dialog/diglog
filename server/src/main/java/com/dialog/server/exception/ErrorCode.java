978764f89afd54a18143326d6327cb6cb6d1ffbc
LeeJaeHoon <dlwogns3413@naver.com>
2025-05-24 20:14:39 +0900

    feat: 토론 북마크, 북마크 삭제 API 구현  (#10)
    * feat: 토론 북마크 api 구현
    
    * refactor: scrap api base url 변경
    
    * feat: 북마크 삭제 api 구현
    
    * refactor: 북마크 생성, 삭제시 예외처리 구현
    
    ---------
    
    Co-authored-by: DoubleKK <138849238+kwonkeonhyeong@users.noreply.github.com>

--- server/src/main/java/com/dialog/server/exception/ErrorCode.java
+++ server/src/main/java/com/dialog/server/exception/ErrorCode.java
@@ -5,6 +5,10 @@ import org.springframework.http.HttpStatus;
 public enum ErrorCode {
     UNEXPECTED_ERROR("1001", "잘못된 요청입니다.", HttpStatus.BAD_REQUEST), // TODO: 예시이므로 ErrorCode 요소가 생기면 삭제
 
+
+    ALREADY_SCRAPPED("5001", "이미 스크랩을 했습니다.", HttpStatus.BAD_REQUEST),
+    NOT_SCRAPPED_YET("5002", "스크랩이 되어 있지 않습니다.", HttpStatus.BAD_REQUEST),
+
     ALREADY_LIKED("5010", "이미 좋아요를 눌렀습니다.", HttpStatus.BAD_REQUEST),
     NOT_LIKED_YET("5011", "좋아요를 누르지 않았습니다.", HttpStatus.BAD_REQUEST),
     ;
