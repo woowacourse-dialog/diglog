80ca0534f99f7c1b37cfbde95bb88ba499783d61
kwonkeonhyeong <rjsgud19566@gmail.com>
2025-05-24 21:15:33 +0900

    merge: conflict 해결

--- server/src/main/java/com/dialog/server/domain/Discussion.java
+++ server/src/main/java/com/dialog/server/domain/Discussion.java
@@ -3,24 +3,15 @@ package com.dialog.server.domain;
 import com.dialog.server.exception.DialogException;
 import com.dialog.server.exception.ErrorCode;
 import jakarta.persistence.*;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.Table;
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
