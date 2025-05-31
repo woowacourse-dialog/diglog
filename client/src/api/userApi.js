0ab579c2dd4d155585f744598db7d93a89314154
LeeJaeHoon <dlwogns3413@naver.com>
2025-05-31 14:23:53 +0900

    feat: api 뼈대 코드 구축

---
+++ client/src/api/userApi.js
@@ -0,0 +1,10 @@
+import api from './axios';
+
+export const userApi = {
+  
+  signup: ({email, password, name, nickname}) => {
+    return api.post('/users/signup', {email, password, name, nickname});
+  },
+};
+
+export default userApi; 
