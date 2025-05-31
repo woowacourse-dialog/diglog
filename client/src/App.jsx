02bc3ec31b9044ecf217b1b742edfa88f8059aea
LeeJaeHoon <dlwogns3413@naver.com>
2025-05-31 14:51:11 +0900

    feat: Header 컴포넌트 구현

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,27 +1,16 @@
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
-import About from './pages/About';
 import './App.css';
 
 function App() {
   return (
     <Router>
       <div className="app">
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-          </ul>
-        </nav>
-
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-        </Routes>
+        <main className="main-content">
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </main>
       </div>
     </Router>
   );
