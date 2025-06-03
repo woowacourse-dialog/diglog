17c766928f103068249214d31d5a28cae5ce4d9c
DongchannN <ldc991104@gmail.com>
2025-06-03 17:57:27 +0900

    feat: 로그인 페이지 구현

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
+import Login from './pages/Login/Login';
+import Signup from './pages/Signup/Signup';
 import './App.css';
 
 function App() {
@@ -10,6 +12,14 @@ function App() {
           <Routes>
             <Route path="/" element={<Home />} />
           </Routes>
+
+            <Routes>
+              <Route path="/login" element={<Login />} />
+            </Routes>
+
+            <Routes>
+              <Route path="/signup" element={<Signup />} />
+            </Routes>
         </main>
       </div>
     </Router>
