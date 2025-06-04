063661d99ca941845ed1acf3b3422a00c2314668
DongchannN <ldc991104@gmail.com>
2025-06-04 15:43:02 +0900

    refactor: CORS 관련 설정 및 개발 단계에서 권한 인증 완화

--- server/src/main/java/com/dialog/server/config/WebSecurityConfig.java
+++ server/src/main/java/com/dialog/server/config/WebSecurityConfig.java
@@ -10,6 +10,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfigurationSource;
 
 @RequiredArgsConstructor
 @Configuration
@@ -19,21 +20,26 @@ public class WebSecurityConfig {
     private final CustomOAuth2UserService customOAuth2UserService;
     private final OAuth2SuccessHandler oAuth2SuccessHandler;
     private final OAuth2FailureHandler oAuth2FailureHandler;
+    private final CorsConfigurationSource corsConfigurationSource;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable)
+                .cors(corsConfigurer -> corsConfigurer.configurationSource(corsConfigurationSource))
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/", "/api/signup", "/api/signup/check").permitAll()
-                        .anyRequest().authenticated()
+                                // TODO: 개발 완료 시 아래 행 삭제
+                                .anyRequest().permitAll()
+                        // TODO: 개발 완료 시 아래 주석 해제
+//                        .requestMatchers("/", "/api/signup", "/api/signup/check").permitAll()
+//                        .anyRequest().authenticated()
                 )
                 .oauth2Login(oAuth2LoginConfigurer -> oAuth2LoginConfigurer
                         .successHandler(oAuth2SuccessHandler)
                         .failureHandler(oAuth2FailureHandler)
                         .userInfoEndpoint(userInfo -> userInfo.userService(customOAuth2UserService)
-                )
-        );
+                        )
+                );
         return http.build();
     }
 }
